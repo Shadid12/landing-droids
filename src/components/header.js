022a54106718f69962722c3ab4d047357eaf8496
sehmim <sehmim2010@gmail.com>
2019-04-26 19:08:38 -0400

    try to do something with the navbar

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,19 @@ import './header.css'
 
 function Header() {
   return (
-    <div className="header">
-      Hello worlds
+    <div>
+      <nav className="nav-bar">
+        <div className="sec-1">
+          <a link="#">Home</a>
+        </div>
+        <div className="sec-2">
+          <a link="#">About Us</a>
+          <a link="#">Blog</a>
+          <a link="#">Team</a>
+        </div>
+      </nav>
+      <div className="header">
+      </div>
     </div>
   );
 }
